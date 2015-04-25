e540623650787afcca2715199018333140b86404
Carlos Saraiva <carlos.saraiva@msn.com>
2015-04-25 20:09:18 +0100

    Codigo refatorado

--- install.js
+++ install.js
@@ -2,23 +2,23 @@ var Service = require('node-windows').Service;
 var wincmd = require('node-windows');
 
 
-wincmd.isAdminUser(function (isAdmin){
-  if (isAdmin) {
-    console.log('Usuário é Administrador!');
-  } else {
-    console.error('Usuário NÃO é Administrador!');
-  }
+wincmd.isAdminUser(function (isAdmin) {
+    if (isAdmin) {
+        console.log('Usuário é Administrador!');
+    } else {
+        console.error('Usuário NÃO é Administrador!');
+    }
 });
 
 // Create a new service object
 var svc = new Service({
-  name:'Logger',
-  description: '',
-  script: require('path').join(__dirname,'router.js'),
-  env:{
-    name: "NODE_ENV",
-    value: "production"
-  }
+    name: 'Logger',
+    description: '',
+    script: require('path').join(__dirname, 'router.js'),
+    env: {
+        name: "NODE_ENV",
+        value: "production"
+    }
 });
 
 // svc.user.domain = 'mydomain.local';
@@ -27,19 +27,19 @@ var svc = new Service({
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
-  svc.start();
+svc.on('install', function () {
+    svc.start();
 });
 
 // Just in case this file is run twice.
-svc.on('alreadyinstalled',function(){
-  console.error('Este serviço já esta instalado no sistema.');
+svc.on('alreadyinstalled', function () {
+    console.error('Este serviço já esta instalado no sistema.');
 });
 
 // Listen for the "start" event and let us know when the
 // process has actually started working.
-svc.on('start',function(){
-  console.log(svc.name+' iniciado...\nServiço disponível na porta 1330.');
+svc.on('start', function () {
+    console.log(svc.name + ' iniciado...\nServiço disponível na porta 1330.');
 });
 
 // Install the script as a service.
--- router.js
+++ router.js
@@ -10,52 +10,59 @@ var EventLogger = require("node-windows").EventLogger;
 var router = new Router();
 var server = http.createServer(router);
 var log = new EventLogger('Logger');
-var litTabelaString = "insert ligacao_telefonica(lit_nome_empresa, lit_data, lit_origem, lit_destino, lit_duracao_total, lit_duracao_conversacao, lit_status, lit_identificador_gravacao, lit_codigo_agilus)";
-var connectionString;
+var tabela = "insert ligacao_telefonica(lit_nome_empresa, lit_data, lit_origem, lit_destino, lit_duracao_total, lit_duracao_conversacao, lit_status, lit_identificador_gravacao, lit_codigo_agilus)";
+var connectionString, connectionErrorFlag;
 
 //Leitura do arquivo de configuração e Inicio do server
-var file = fs.readFile(path.join(__dirname, "sql.udl"), "ucs2", function (fileError, data) {
-    if (fileError) {
-        console.log("Arquivo de configuração não encontrado ou corrompido. Server não pode ser iniciado.");
-        log.error("Arquivo de configuração não encontrado ou corrompido. Server não pode ser iniciado.", 1000, function () {
-            process.exit(1);
-        });
-    } else {
-        var udl = JSON.parse(UDLtoJSON(data));
-        connectionString = {
-            user: udl.UserID !== undefined ? udl.UserID : "",
-            password: udl.Password !== undefined ? udl.Password : "",
-            server: udl.DataSource !== undefined ? udl.DataSource : "",
-            database: udl.InitialCatalog !== undefined ? udl.InitialCatalog : "",
-            appName: "Logger"
-        };
-        //Inicio do server
-        server.listen(1330, function () {
-            log.info("Server iniciado na porta 1330");
-        });
-    }
+readUDL("agilus.udl", function () {
+    server.listen(1330, function () {
+        log.info("Server iniciado na porta 1330");
+    });
 });
 
-//Rotes
+//Rotas
 router.post("/insert", function (request, response) {
-    var query = queryString(request);
+    var query = insertString(request);
     database(query, function (result) {
         response.end(JSON.stringify(result));
     });
 });
 
 //Funções auxiliares
+//Leitura do arquivo de configuração
+function readUDL(file, callback) {
+    var f = fs.readFile(path.join(__dirname, file), "ucs2", function (fileError, data) {
+        if (fileError) {
+            console.log("Arquivo de configuração não encontrado ou corrompido. Server não pode ser iniciado.");
+            log.error("Arquivo de configuração não encontrado ou corrompido. Server não pode ser iniciado.", 1000, function () {
+                process.exit(1);
+            });
+        } else {
+            var udl = JSON.parse(UDLtoJSON(data));
+            connectionString = {
+                user: udl.UserID !== undefined ? udl.UserID : "",
+                password: udl.Password !== undefined ? udl.Password : "",
+                server: udl.DataSource !== undefined ? udl.DataSource : "",
+                database: udl.InitialCatalog !== undefined ? udl.InitialCatalog : "",
+                appName: "Logger"
+            };
+            callback();
+        }
+    });
+}
+
 //Monta a string do insert que sera executado no banco de dados
-function queryString(request) {
-    return "'" + request.post.nome + "'," +
-        "'" + request.post.calldate + "," +
-        "'" + request.post.src + "," +
-        "'" + request.post.dst + "," +
-        "'" + request.post.duration + "," +
-        "'" + request.post.billsec + "," +
-        "'" + request.post.disposition + "," +
-        "'" + request.post.userfield + "," +
-        "'" + request.post.callid;
+function insertString(request) {
+    var values = "'" + request.post.nome + "'," +
+        "'" + request.post.calldate + "'," +
+        "'" + request.post.src + "'," +
+        "'" + request.post.dst + "'," +
+        "'" + request.post.duration + "'," +
+        "'" + request.post.billsec + "'," +
+        "'" + request.post.disposition + "'," +
+        "'" + request.post.userfield + "'," +
+        "'" + request.post.callid + "'";
+    return tabela + "values(" + values + ")";
 }
 
 //Função responsavel por conectar no banco de dados e fazer a inserção
@@ -67,14 +74,12 @@ function database(query, callback) {
                 if (!queryError) {
                     callback("Ok");
                 } else {
-                    console.log(queryError.name + ": " + queryError.message);
                     log.error(queryError.name + ": " + queryError.message, 1000);
                     callback(queryError);
                 }
                 connection.close();
             });
         } else {
-            console.log(JSON.stringify(connectionError.name + ": " + connectionError.message));
             log.error(connectionError.name + ": " + connectionError.message, 1000);
             callback(connectionError);
         }
@@ -89,6 +94,5 @@ function UDLtoJSON(data) {
         var prop = data[item].replace(/[|]|\n|\r| | [oledb]/g, '').split("=");
         udl += prop.length > 1 ? '"' + prop[0] + '"' + ': ' + '"' + prop[1] + '"' + ',' : '"' + prop[0] + '"' + ': ' + '"' + '' + '"' + ',';
     }
-    console.log(udl);
     return "{" + udl.slice(0, udl.length - 1) + "}";
 }
