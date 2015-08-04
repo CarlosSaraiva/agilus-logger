1677929e7dd2726aa2709736b5af1ce234e484a1
Carlos Saraiva <carlos.saraiva@msn.com>
2015-08-04 01:40:37 -0300

    Minimist added. Services are created dynamically.

--- agilus-logger.js
+++ agilus-logger.js
@@ -5,7 +5,6 @@ var sql = require("mssql");
 var fs = require("fs");
 var path = require("path");
 var EventLogger = require("node-windows").EventLogger;
-var minimist = require("minimist");
 
 //Instanciando objetos e declarando/iniciando variáveis
 var router = new Router();
@@ -14,13 +13,13 @@ var log = new EventLogger("Agilus Logger");
 var tabela = "insert ligacao_telefonica(lit_nome_empresa, lit_data, lit_origem, lit_destino, lit_duracao_total, lit_duracao_conversacao, lit_status, lit_identificador_gravacao, lit_codigo_agilus)";
 var connectionString, connectionErrorFlag;
 
-//Argumentos do prompt
-console.log(minimist);
+//Variaveis do usuario
+var port = process.env.p;
 
 //Leitura do arquivo de configuração e Inicio do server
 readUDL("agilus.udl", function () {
-    server.listen(1330, "0.0.0.0", function () {
-        log.info("Server iniciado na porta 1330");
+    server.listen(port, "0.0.0.0", function () {
+        log.info("Server iniciado na porta: " + port);
     });
 });
 
--- install.js
+++ install.js
@@ -1,37 +1,40 @@
-var Service = require('node-windows').Service;
-var wincmd = require('node-windows');
-
+var Service = require("node-windows").Service;
+var wincmd = require("node-windows");
+var argv = require("minimist")(process.argv.slice(2));
 
 // Create a new service object
 var svc = new Service({
-    name: 'Agilus Logger',
-    description: '',
-    script: require('path').join(__dirname, 'agilus-logger.js'),
-    env: {
-        name: "NODE_ENV",
-        value: "production"
-    }
+    name: argv.e + " Logger",
+    description: "",
+    script: require("path").join(__dirname, "agilus-logger.js"),
+    env: [
+        {
+            name: "NODE_ENV",
+            value: "production"
+        },
+        {
+            name: "p",
+            value: argv.p
+        }
+    ],
+    exe:  "logger-" + argv.e
 });
-
-// svc.user.domain = 'mydomain.local';
-// svc.user.account = 'Carlos';
-// svc.user.password = 'sde9016207946';
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install', function () {
+svc.on("install", function () {
     svc.start();
 });
 
 // Just in case this file is run twice.
-svc.on('alreadyinstalled', function () {
-    console.error('Este serviço já esta instalado no sistema.');
+svc.on("alreadyinstalled", function () {
+    console.error("Este serviço já esta instalado no sistema.");
 });
 
 // Listen for the "start" event and let us know when the
 // process has actually started working.
-svc.on('start', function () {
-    console.log(svc.name + ' iniciado...\nServiço disponível na porta 1330.');
+svc.on("start", function () {
+    console.log(svc.name + " iniciado...\nServiço disponível na porta: " + argv.p + " em: " + argv.e);
 });
 
 // Install the script as a service.
