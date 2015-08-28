3f6c51dee39558e5486fa3e140661d536a1d88fe
Carlos Saraiva <carlos.saraiva@msn.com>
2015-08-28 01:31:23 -0300

    Update (3)

--- AgilusLogger/Node.cs
+++ AgilusLogger/Node.cs
@@ -1,40 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AgilusLogger
 {
-    public class NodeAction
-    {
-        private NodeAction(string value, int id)
-        {
-            Value = value;
-            Id = id;
-        }
-
-        public string Value { get; set; }
-
-        public int Id { get; set; }
-
-        public static NodeAction Install => new NodeAction("Install", 1);
-
-        public static NodeAction Uninstall => new NodeAction("Uninstall", 2);
-
-        public static NodeAction Update => new NodeAction(@"Update", 3);
-    }
-
-    public static  class Node
+    public static class Node
     {
         private static Process _process;
 
-        private static event EventHandler OnBeginInstall;
-
         public static event EventHandler OnExit;
+
+        private static event EventHandler OnBeginInstall;
 
         public static async Task ExecuteNodeAsync(NodeAction command, object service)
         {
@@ -45,12 +22,15 @@ namespace AgilusLogger
                 case 1:
                     flags = $"-n {service.name} -p {service.port}";
                     break;
+
                 case 2:
                     flags = $"{service.name}";
                     break;
+
                 case 3:
                     flags = String.Empty;
                     break;
+
                 default:
                     flags = String.Empty;
                     break;
@@ -68,11 +48,26 @@ namespace AgilusLogger
                     RedirectStandardOutput = true
                 }
             };
-            {                
+            {
                 _process.Start();
                 OnBeginInstall?.Invoke(null, new EventArgs());
                 OnExit?.Invoke(null, EventArgs.Empty);
             }
         }
+    }
+
+    public class NodeAction
+    {
+        private NodeAction(string value, int id)
+        {
+            Value = value;
+            Id = id;
+        }
+
+        public int Id { get; set; }
+        public static NodeAction Install => new NodeAction("Install", 1);
+        public static NodeAction Uninstall => new NodeAction("Uninstall", 2);
+        public static NodeAction Update => new NodeAction(@"Update", 3);
+        public string Value { get; set; }
     }
 }
