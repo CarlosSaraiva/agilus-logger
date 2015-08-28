087e6121a3aafdd6253df11d8eefd6dce19ce0de
Carlos Saraiva <carlos.saraiva@msn.com>
2015-08-28 01:24:07 -0300

    Fixing git problems(2)

--- AgilusLogger/Node.cs
+++ AgilusLogger/Node.cs
@@ -67,7 +67,6 @@ namespace AgilusLogger
                     CreateNoWindow = false,
                     RedirectStandardOutput = true
                 }
-
             };
             {                
                 _process.Start();
