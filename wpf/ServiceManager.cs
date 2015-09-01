fc4f5f44e7d6070fdd9b7f2b164e348f84f3622b
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-01 19:18:51 -0300

    Document

--- wpf/ServiceManager.cs
+++ wpf/ServiceManager.cs
@@ -12,29 +12,93 @@ namespace AgilusLogger
 {
     using static ServiceController;
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class ServiceManager
     {
-        //Events
+        /// <summary>
+        /// Occurs when [on service list updated].
+        /// </summary>
         public event EventHandler OnServiceListUpdated;
 
+        /// <summary>
+        /// Occurs when [on tick].
+        /// </summary>
         public event EventHandler OnTick;
 
+        /// <summary>
+        /// Occurs when [on stopped service].
+        /// </summary>
         public event EventHandler OnStoppedService;
 
-        //Fields-Properties
+        /// <summary>
+        /// Occurs when [_on test].
+        /// </summary>
+        private event EventHandler _onTest;
+
+        /// <summary>
+        /// Occurs when [on test].
+        /// </summary>
+        public event EventHandler OnTest
+        {
+            add
+            {
+                
+                _onTest += value;
+            }
+            remove
+            {
+                _onTest -= value;
+            }
+        }
+
+        /// <summary>
+        /// The _regex
+        /// </summary>
         private readonly Regex _regex;
 
+        /// <summary>
+        /// Gets the filter.
+        /// </summary>
+        /// <value>
+        /// The filter.
+        /// </value>
         private string Filter { get; }
 
+        /// <summary>
+        /// Gets the ticks.
+        /// </summary>
+        /// <value>
+        /// The ticks.
+        /// </value>
         private long Ticks { get; }
 
+        /// <summary>
+        /// Gets or sets the update timer.
+        /// </summary>
+        /// <value>
+        /// The update timer.
+        /// </value>
         private DispatcherTimer UpdateTimer { get; set; }
 
+        /// <summary>
+        /// The _history
+        /// </summary>
         private readonly Queue<IEnumerable<LogItem>> _history;
 
+        /// <summary>
+        /// Gets the loggers.
+        /// </summary>
+        /// <value>
+        /// The loggers.
+        /// </value>
         public ObservableCollection<LoggerService> Loggers { get; private set; }
 
-        //Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceManager"/> class.
+        /// </summary>
+        /// <param name="ticks">The ticks.</param>
         public ServiceManager(long ticks)
         {
             Ticks = ticks;
@@ -45,8 +109,10 @@ namespace AgilusLogger
             _history = new Queue<IEnumerable<LogItem>>();
         }
 
-        //Methods
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0022:Should dispose object", Justification = "<Pending>")]
+        /// <summary>
+        /// Gets the filtered services.
+        /// </summary>
+        /// <returns></returns>
         private ObservableCollection<LoggerService> GetFilteredServices()
         {
             var result = from service in GetServices().ToList()
@@ -56,6 +122,9 @@ namespace AgilusLogger
             return new ObservableCollection<LoggerService>(result);
         }
 
+        /// <summary>
+        /// Sets the timer.
+        /// </summary>
         private void SetTimer()
         {
             UpdateTimer = new DispatcherTimer
@@ -88,6 +157,7 @@ namespace AgilusLogger
             }
 
             OnTick?.Invoke(this, new EventArgs());
+            _onTest?.Invoke(this, new EventArgs());
         }
 
         public IEnumerable<LoggerService> GetStoppedServices => Loggers.Where(l => l.Status.ToString() == "Stopped");
@@ -95,10 +165,36 @@ namespace AgilusLogger
 
     public class LogItem : IEquatable<LogItem>
     {
+        /// <summary>
+        /// Gets the name of the entity.
+        /// </summary>
+        /// <value>
+        /// The name of the entity.
+        /// </value>
         private string EntityName { get; }
+
+        /// <summary>
+        /// Gets the port.
+        /// </summary>
+        /// <value>
+        /// The port.
+        /// </value>
         private int Port { get; }
+
+        /// <summary>
+        /// Gets the status.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
         private string Status { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogItem" /> class.
+        /// </summary>
+        /// <param name="entityName">Name of the entity.</param>
+        /// <param name="port">The port.</param>
+        /// <param name="status">The status.</param>
         public LogItem(string entityName, int port, string status)
         {
             EntityName = entityName;
@@ -106,6 +202,11 @@ namespace AgilusLogger
             Status = status;
         }
 
+        /// <summary>
+        /// Equalses the specified other.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns></returns>
         public bool Equals(LogItem other)
         {
             if (ReferenceEquals(other, null)) return false;
@@ -113,6 +214,12 @@ namespace AgilusLogger
             return EntityName.Equals(other.EntityName) && Port.Equals(other.Port) && Status.Equals(other.Status);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             var hasEntityName = EntityName?.GetHashCode() ?? 0;
